ff121b93640739b01061121235eeeb0d32bb1959
Netkas <45276434+Netkas@users.noreply.github.com>
2019-11-27 22:10:03 -0300

    Added app config files

--- src/resources/sections/header.php
+++ src/resources/sections/header.php
@@ -1,19 +1,23 @@
-<!-- Required meta tags -->
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<!-- plugins:css -->
+<meta name="apple-mobile-web-app-title" content="Intellivoid Staff">
+<meta name="application-name" content="Intellivoid Staff">
+<meta name="msapplication-TileColor" content="#0030eb">
+<meta name="msapplication-TileImage" content="/assets/favicon/mstile-144x144.png?v=xQOrL7ldK2">
+<meta name="msapplication-config" content="/assets/favicon/browserconfig.xml?v=xQOrL7ldK2">
+<meta name="theme-color" content="#0030eb">
 <link rel="stylesheet" href="/assets/vendors/iconfonts/mdi/css/materialdesignicons.min.css">
 <link rel="stylesheet" href="/assets/vendors/iconfonts/puse-icons-feather/feather.css">
 <link rel="stylesheet" href="/assets/vendors/css/vendor.bundle.base.css">
 <link rel="stylesheet" href="/assets/vendors/css/vendor.bundle.addons.css">
-<!-- endinject -->
-<!-- plugin css for this page -->
-<!-- End plugin css for this page -->
-<!-- inject:css -->
 <link rel="stylesheet" href="/assets/css/shared/style.css">
-<!-- endinject -->
-<!-- Layout styles -->
 <link rel="stylesheet" href="/assets/css/demo_1/style.css">
 <link rel="stylesheet" href="/assets/css/demo_3/dark-theme.css">
-<!-- End Layout styles -->
-<link rel="shortcut icon" href="/assets/images/favicon.png" />
+<link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png?v=xQOrL7ldK2">
+<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png?v=xQOrL7ldK2">
+<link rel="icon" type="image/png" sizes="194x194" href="/assets/favicon/favicon-194x194.png?v=xQOrL7ldK2">
+<link rel="icon" type="image/png" sizes="192x192" href="/assets/favicon/android-chrome-192x192.png?v=xQOrL7ldK2">
+<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png?v=xQOrL7ldK2">
+<link rel="manifest" href="/assets/favicon/site.webmanifest?v=xQOrL7ldK2">
+<link rel="mask-icon" href="/assets/favicon/safari-pinned-tab.svg?v=xQOrL7ldK2" color="#0030eb">
+<link rel="shortcut icon" href="/assets/favicon/favicon.ico?v=xQOrL7ldK2">
