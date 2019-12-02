0b36e9e1c1b0c6def934308ff0f83e12b794fafb
Netkas <45276434+Netkas@users.noreply.github.com>
2019-12-02 12:45:02 -0300

    Corrected headers

--- src/resources/sections/login_headers.php
+++ src/resources/sections/login_headers.php
@@ -1,9 +1,22 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="apple-mobile-web-app-title" content="Intellivoid Staff">
+<meta name="application-name" content="Intellivoid Staff">
+<meta name="msapplication-TileColor" content="#252C46">
+<meta name="msapplication-TileImage" content="/assets/favicon/mstile-144x144.png?v=xQOrL7ldK2">
+<meta name="msapplication-config" content="/assets/favicon/browserconfig.xml?v=xQOrL7ldK2">
+<meta name="theme-color" content="#252C46">
 <link rel="stylesheet" href="/assets/vendors/iconfonts/mdi/css/materialdesignicons.min.css">
 <link rel="stylesheet" href="/assets/vendors/iconfonts/puse-icons-feather/feather.css">
 <link rel="stylesheet" href="/assets/vendors/css/vendor.bundle.base.css">
 <link rel="stylesheet" href="/assets/vendors/css/vendor.bundle.addons.css">
 <link rel="stylesheet" href="/assets/css/shared/style.css">
 <link rel="stylesheet" href="/assets/css/extra.css">
-<link rel="stylesheet" href="/assets/css/animate.css">
+<link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png?v=xQOrL7ldK2">
+<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png?v=xQOrL7ldK2">
+<link rel="icon" type="image/png" sizes="194x194" href="/assets/favicon/favicon-194x194.png?v=xQOrL7ldK2">
+<link rel="icon" type="image/png" sizes="192x192" href="/assets/favicon/android-chrome-192x192.png?v=xQOrL7ldK2">
+<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png?v=xQOrL7ldK2">
+<link rel="manifest" href="/assets/favicon/site.webmanifest?v=xQOrL7ldK2">
+<link rel="mask-icon" href="/assets/favicon/safari-pinned-tab.svg?v=xQOrL7ldK2" color="#0030eb">
+<link rel="shortcut icon" href="/assets/favicon/favicon.ico?v=xQOrL7ldK2">
