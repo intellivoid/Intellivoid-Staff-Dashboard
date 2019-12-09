ae12550d659f76a7cccda4cd21ed4691fb58692b
Netkas <45276434+Netkas@users.noreply.github.com>
2019-12-09 16:59:52 -0300

    Fixed imports for 'otl_codes'

--- src/resources/pages/otl_codes/contents.php
+++ src/resources/pages/otl_codes/contents.php
@@ -3,17 +3,10 @@
     use DynamicalWeb\DynamicalWeb;
     use DynamicalWeb\HTML;
     use DynamicalWeb\Runtime;
-    use IntellivoidAccounts\Abstracts\AccountRequestPermissions;
-use IntellivoidAccounts\Abstracts\ApplicationFlags;
-use IntellivoidAccounts\Abstracts\ApplicationStatus;
-use IntellivoidAccounts\Abstracts\AuthenticationRequestStatus;
-use IntellivoidAccounts\Abstracts\OtlStatus;
-use IntellivoidAccounts\Abstracts\SearchMethods\ApplicationSearchMethod;
-use IntellivoidAccounts\IntellivoidAccounts;
-use IntellivoidAccounts\Objects\COA\Application;
-use msqg\Abstracts\SortBy;
-use msqg\QueryBuilder;
-    use ZiProto\ZiProto;
+    use IntellivoidAccounts\Abstracts\OtlStatus;
+    use IntellivoidAccounts\IntellivoidAccounts;
+    use msqg\Abstracts\SortBy;
+    use msqg\QueryBuilder;
 
     Runtime::import('IntellivoidAccounts');
     HTML::importScript('db_render_helper');
