a20c696b4cf260db60a526a326304f4cebc53399
Netkas <45276434+Netkas@users.noreply.github.com>
2019-12-09 17:10:17 -0300

    Fixed imports for 'application_access'

--- src/resources/pages/application_access/contents.php
+++ src/resources/pages/application_access/contents.php
@@ -4,16 +4,12 @@
     use DynamicalWeb\HTML;
     use DynamicalWeb\Runtime;
     use IntellivoidAccounts\Abstracts\AccountRequestPermissions;
-use IntellivoidAccounts\Abstracts\ApplicationAccessStatus;
-use IntellivoidAccounts\Abstracts\ApplicationFlags;
-use IntellivoidAccounts\Abstracts\ApplicationStatus;
-use IntellivoidAccounts\Abstracts\AuthenticationAccessStatus;
-use IntellivoidAccounts\Abstracts\AuthenticationRequestStatus;
-use IntellivoidAccounts\Abstracts\SearchMethods\ApplicationSearchMethod;
-use IntellivoidAccounts\IntellivoidAccounts;
-use IntellivoidAccounts\Objects\COA\Application;
-use msqg\Abstracts\SortBy;
-use msqg\QueryBuilder;
+    use IntellivoidAccounts\Abstracts\ApplicationAccessStatus;
+    use IntellivoidAccounts\Abstracts\SearchMethods\ApplicationSearchMethod;
+    use IntellivoidAccounts\IntellivoidAccounts;
+    use IntellivoidAccounts\Objects\COA\Application;
+    use msqg\Abstracts\SortBy;
+    use msqg\QueryBuilder;
     use ZiProto\ZiProto;
 
     Runtime::import('IntellivoidAccounts');
