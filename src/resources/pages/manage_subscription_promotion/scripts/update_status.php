b765f54a53707047b45394d4422822dc13005c1d
Netkas <45276434+Netkas@users.noreply.github.com>
2020-01-10 10:56:52 -0300

    Added the ability to manage subscription promotions

---
+++ src/resources/pages/manage_subscription_promotion/scripts/update_status.php
@@ -0,0 +1,65 @@
+<?php
+
+    use DynamicalWeb\Actions;
+    use DynamicalWeb\DynamicalWeb;
+    use IntellivoidAccounts\Abstracts\SubscriptionPlanStatus;
+    use IntellivoidAccounts\IntellivoidAccounts;
+    use IntellivoidAccounts\Objects\SubscriptionPlan;
+
+    if(isset($_GET['action']))
+    {
+        if($_GET['action'] == 'update_status')
+        {
+            if($_SERVER['REQUEST_METHOD'] == 'POST')
+            {
+                try
+                {
+                    update_status();
+                }
+                catch(Exception $e)
+                {
+                    Actions::redirect(DynamicalWeb::getRoute('manage_subscription_plan',
+                        array('id' => $_GET['id'], 'callback' => '101')
+                    ));
+                }
+            }
+        }
+    }
+
+    function update_status()
+    {
+        /** @var SubscriptionPlan $SubscriptionPlan */
+        $SubscriptionPlan = DynamicalWeb::getMemoryObject('subscription_plan');
+
+        /** @var IntellivoidAccounts $IntellivoidAccounts */
+        $IntellivoidAccounts = DynamicalWeb::getMemoryObject("intellivoid_accounts");
+
+        if(isset($_POST['status']) == false)
+        {
+            Actions::redirect(DynamicalWeb::getRoute('manage_subscription_plan',
+                array('id' => $_GET['id'], 'callback' => '100')
+            ));
+        }
+
+        switch($_POST['status'])
+        {
+            case 'available':
+                $SubscriptionPlan->Status = SubscriptionPlanStatus::Available;
+                break;
+
+            case 'unavailable':
+                $SubscriptionPlan->Status = SubscriptionPlanStatus::Unavailable;
+                break;
+
+            default:
+                Actions::redirect(DynamicalWeb::getRoute('manage_subscription_plan',
+                    array('id' => $_GET['id'], 'callback' => '104')
+                ));
+        }
+
+        $IntellivoidAccounts->getSubscriptionPlanManager()->updateSubscriptionPlan($SubscriptionPlan);
+
+        Actions::redirect(DynamicalWeb::getRoute('manage_subscription_plan',
+            array('id' => $_GET['id'])
+        ));
+    }
